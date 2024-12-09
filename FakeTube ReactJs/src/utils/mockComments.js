87f1f988f414aa05084847f23eb015c490d08a77
Abhishek Kaplesh <abkaplesh02@gmail.com>
2024-12-10 01:42:29 +0530

    update files

--- FakeTube ReactJs/src/utils/mockComments.js
+++ FakeTube ReactJs/src/utils/mockComments.js
@@ -90,6 +90,121 @@ const mockComments=[
             
         ]
     }
+    ,
+    {
+        name: "Akshay saini",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas magni corrupti odit molestias eveniet. Cum, architecto! Nesciunt rem ab eveniet, praesentium aut illum qui consequatur, soluta inventore molestias architecto. Nesciunt!",
+        replies:[
+            {
+                name: "pehla reply",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas magni corrupti odit molestias eveniet. Cum, architecto! Nesciunt rem ab eveniet, praesentium aut illum qui consequatur, soluta inventore molestias architecto. Nesciunt!",
+                replies:[
+                    {
+                        name: "doosra saini",
+                        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas magni corrupti odit molestias eveniet. Cum, architecto! Nesciunt rem ab eveniet, praesentium aut illum qui consequatur, soluta inventore molestias architecto. Nesciunt!",
+                        replies:[
+                            
+                        ]
+                    },
+                    
+                ]
+            },
+
+        ]
+    },
+    {
+        name: "Abhishek  saini",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas magni corrupti odit molestias eveniet. Cum, architecto! Nesciunt rem ab eveniet, praesentium aut illum qui consequatur, soluta inventore molestias architecto. Nesciunt!",
+        replies:[
+            {
+                name: "pehla saini",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas magni corrupti odit molestias eveniet. Cum, architecto! Nesciunt rem ab eveniet, praesentium aut illum qui consequatur, soluta inventore molestias architecto. Nesciunt!",
+                replies:[
+                    {
+                        name: "pehle reply ka reply saini",
+                        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas magni corrupti odit molestias eveniet. Cum, architecto! Nesciunt rem ab eveniet, praesentium aut illum qui consequatur, soluta inventore molestias architecto. Nesciunt!",
+                        replies:[
+                            {
+                                name: "pehle reply ke reply ka reply saini",
+                                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas magni corrupti odit molestias eveniet. Cum, architecto! Nesciunt rem ab eveniet, praesentium aut illum qui consequatur, soluta inventore molestias architecto. Nesciunt!",
+                                replies:[
+                                    {
+                                        name: "pehle reply ke reply ke reply ka reply ka reply",
+                                        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas magni corrupti odit molestias eveniet. Cum, architecto! Nesciunt rem ab eveniet, praesentium aut illum qui consequatur, soluta inventore molestias architecto. Nesciunt!",
+                                        replies:[
+                                            
+                                        ]
+                                    },
+                                    
+                                ]
+                            },
+                            
+                        ]
+                    },
+                    
+                ]
+            },
+            
+        ]
+    }
+
+    ,
+    {
+        name: "Akshay saini",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas magni corrupti odit molestias eveniet. Cum, architecto! Nesciunt rem ab eveniet, praesentium aut illum qui consequatur, soluta inventore molestias architecto. Nesciunt!",
+        replies:[
+            {
+                name: "pehla reply",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas magni corrupti odit molestias eveniet. Cum, architecto! Nesciunt rem ab eveniet, praesentium aut illum qui consequatur, soluta inventore molestias architecto. Nesciunt!",
+                replies:[
+                    {
+                        name: "doosra saini",
+                        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas magni corrupti odit molestias eveniet. Cum, architecto! Nesciunt rem ab eveniet, praesentium aut illum qui consequatur, soluta inventore molestias architecto. Nesciunt!",
+                        replies:[
+                            
+                        ]
+                    },
+                    
+                ]
+            },
+
+        ]
+    },
+    {
+        name: "Abhishek  saini",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas magni corrupti odit molestias eveniet. Cum, architecto! Nesciunt rem ab eveniet, praesentium aut illum qui consequatur, soluta inventore molestias architecto. Nesciunt!",
+        replies:[
+            {
+                name: "pehla saini",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas magni corrupti odit molestias eveniet. Cum, architecto! Nesciunt rem ab eveniet, praesentium aut illum qui consequatur, soluta inventore molestias architecto. Nesciunt!",
+                replies:[
+                    {
+                        name: "pehle reply ka reply saini",
+                        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas magni corrupti odit molestias eveniet. Cum, architecto! Nesciunt rem ab eveniet, praesentium aut illum qui consequatur, soluta inventore molestias architecto. Nesciunt!",
+                        replies:[
+                            {
+                                name: "pehle reply ke reply ka reply saini",
+                                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas magni corrupti odit molestias eveniet. Cum, architecto! Nesciunt rem ab eveniet, praesentium aut illum qui consequatur, soluta inventore molestias architecto. Nesciunt!",
+                                replies:[
+                                    {
+                                        name: "pehle reply ke reply ke reply ka reply ka reply",
+                                        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas magni corrupti odit molestias eveniet. Cum, architecto! Nesciunt rem ab eveniet, praesentium aut illum qui consequatur, soluta inventore molestias architecto. Nesciunt!",
+                                        replies:[
+                                            
+                                        ]
+                                    },
+                                    
+                                ]
+                            },
+                            
+                        ]
+                    },
+                    
+                ]
+            },
+            
+        ]
+    }
     
 ];
 
